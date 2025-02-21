60f8a71eba59355bfc21d5b3f6b08675860d0267
lê thị thanh hằng <127948428+hangxinh0812@users.noreply.github.com>
2025-02-21 16:11:36 +0700

    xoa bo

--- app/src/main/java/tiil/edu/helloandroid/MainActivity.kt
+++ app/src/main/java/tiil/edu/helloandroid/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
+        se
             HelloAndroidTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
