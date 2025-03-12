2862c3d5cc14dd944c93622e31a5fd065233a371
lê thị thanh hằng <127948428+hangxinh0812@users.noreply.github.com>
2025-03-12 14:38:14 +0700

    push

--- ManHinhDonGian/app/src/main/java/MainActivity.java
+++ ManHinhDonGian/app/src/main/java/MainActivity.java
@@ -1,2 +1,35 @@
-public class MainActivity {
+package tiil.edu.manhinhdongian;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Ánh xạ button và thêm sự kiện chuyển màn hình
+        Button btnMH1 = findViewById(R.id.btnMH1);
+        Button btnMH2 = findViewById(R.id.btnMH2);
+
+        btnMH1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MH1Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnMH2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MH2Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
