47f1172f91cdf4f03e1bbd591da272d242462fd4
lê thị thanh hằng <127948428+hangxinh0812@users.noreply.github.com>
2025-04-21 12:20:02 +0700

    up

--- FragmentDynamicApp/app/src/main/java/tiil/ntu/fragmentdynamicapp/MainActivity.java
+++ FragmentDynamicApp/app/src/main/java/tiil/ntu/fragmentdynamicapp/MainActivity.java
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +22,10 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        FragmentManager fragmentManager =getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .add(R.id.fragmentcontenthoder, new ContentFragment())
+                .add(R.id.fragmentfooterhoder, new FooterFragment())
+                .commit()
     }
 }
